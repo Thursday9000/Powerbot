b15356bc87acf185c32afea7b8c73f1e30edbc29
Aidan <owasaidan88@gmail.com>
2013-05-27 19:47:12 -0400

    .

--- moneyMaker/thurssAIOMoneyMaker/AIOMoneyMaker.java
+++ moneyMaker/thurssAIOMoneyMaker/AIOMoneyMaker.java
@@ -141,7 +141,7 @@ public class AIOMoneyMaker extends ActiveScript implements PaintListener {
 
 	@Override
 	public int loop() {
-		for (final Node job :(new Node[NODES.size()])) {
+		for (final Node job : NODES.toArray(new Node[NODES.size()])) {
 			if (job.activate()) {
 				getContainer().submit(job);
 				job.join();
