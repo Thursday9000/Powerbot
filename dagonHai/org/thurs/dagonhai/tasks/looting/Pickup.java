db0cfd3b360698ef2a3c82f02d7268dfb4263be9
Thursday9000 <straightout6@gmail.com>
2014-08-01 20:22:53 -0400

    updated pickup class

--- dagonHai/org/thurs/dagonhai/tasks/looting/Pickup.java
+++ dagonHai/org/thurs/dagonhai/tasks/looting/Pickup.java
@@ -6,22 +6,21 @@ import org.powerbot.script.rt6.GroundItem;
 import org.thurs.dagonhai.tasks.Task;
 
 public class Pickup extends Task<ClientContext> {
-    public static boolean profit = false;
-	
+	final GroundItem loot = ctx.groundItems.id(557, 556, 559, 554, 555, 558)
+			.nearest().poll();
+	public static boolean profit = false;
+
 	public Pickup(ClientContext ctx) {
 		super(ctx);
 	}
 
 	public boolean activate() {
 		return ctx.backpack.select().count() <= 27
-				&& ctx.groundItems
-						.select()
-						.id(557, 556, 559, 554, 555, 558)
-						.nearest().peek().valid() && profit;
+				&& !ctx.groundItems.select().id(557, 556, 559, 554, 555, 558)
+						.isEmpty() && profit;
 	}
 
 	public void execute() {
-		final GroundItem loot = ctx.groundItems.select().id(557, 556, 559, 554, 555, 558).nearest().poll();
 		if (loot.valid()) {
 			if (loot.inViewport()) {
 				loot.interact("Take", loot.name());
